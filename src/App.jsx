b74aff1b68b96c9d7902e8c522afb331d838b260
Tilherme <gui.mauricio@hotmail.com.br>
2022-07-05 22:46:36 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import {Post} from "./Post"
+import { Header } from './components/Header'
+
+import './global.css'
+import styles from './App.module.css'
+import { Sidebar } from "./components/Sidebar"
+export function App() {
+  return (
+      <div>
+        <Header />
+        <div className={styles.wrapper}>
+        <Sidebar/>
+          <main> 
+          <Post
+         author="Guilherme Mateus"
+         content="Entre as medidas estão projetos já aprovados pelos 3 poderes e que afetam tanto pela perda na arrecadação como pelo aumento de gastos. "
+        />
+        <Post author="Gustavo" content="Outro Post" />
+
+          </main>
+        </div>
+      </div>
+
+    )
+}
+
